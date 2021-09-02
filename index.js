8bc966550006c81e7b15b7d4a9be0cc10360e985
Pallab <mustakimpallab2434@gmail.com>
2021-09-02 23:31:25 +0600

    third commit

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const getElement=document.getElementById('button-addon2').addEventListener("clic
     console.log(searchText);
     search(searchText);
     document.getElementById('searchbox').value="";
+    const div=document.createElement('div');
+      div.classList.add('col');
 });
 
 const search=searchText =>{
@@ -20,8 +22,16 @@ const search=searchText =>{
 const displayBooks=(book)=>{
     const searchResult= document.getElementById('searchResults');
     const numberOfResults=document.getElementById('number');
-    numberOfResults.innerHTML=
+    if(book.numFound){
+
+        numberOfResults.innerHTML=
     `<h5>Number Of books Found: ${book.numFound} ` ;
+    }
+    else{
+        numberOfResults.innerHTML=
+    `<h5> Your Search item is not found in our library ` ;
+    }
+    
     
     book.docs.forEach(data=> {
        console.log(data);
@@ -67,15 +77,55 @@ const displayBooks=(book)=>{
       else {
           authors='Unknown Author';
       }
+
+      let publishers;
+      counter=0;
+      if(data.publisher)
+      {
+        publishers=data.publisher[0]; 
+
+        data.publisher.forEach(publisher=>{
+            counter++;
+            if(data.publisher.length>1 && counter==1)
+            {
+                publishers+=', ';
+            }
+            else if(data.publisher.length==1 && counter==1)
+            {
+                
+            }
+            else if(counter==data.publisher.length)
+            {
+                publishers+=publisher;
+            }
+           else{
+                publishers+=publisher+', ';
+            }
+        })
+      }
+      else {
+          publishers='Unknown';
+      }
       
+      let firstPublishYear;
+      if(data.first_publish_year)
+      {
+        firstPublishYear= data.first_publish_year;
+      }
+      else{
+         firstPublishYear= 'Unknown year :(';
+      }
     
       div.innerHTML=`
           <div class="card h-100">
             <img src="https://covers.openlibrary.org/b/id/${data.cover_i}-M.jpg" class="card-img-top h-100" alt="...">
             <div class="card-body">
-              <h3 class="card-title">${title}</h3>
+              <h1 class="card-title">${title}</h1>
               
-              <h5 class="card-title">By ${authors}</h5>
+              <h4 class="card-title">By ${authors}</h4>
+              <h6 class="card-title">Publisher(s):  ${publishers}</h6>
+              <h6 class="card-title">First Published On ${firstPublishYear}</h6>
+
 
               <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             </div>
